63048e05cc7ab61497b251b3c8c46eb40f091980
sohiekim65 <sohiekim65@gmail.com>
2022-10-15 13:00:07 +0900

    commit

--- src/DivisionAB.java
+++ src/DivisionAB.java
@@ -1,7 +1,7 @@
 // 문제
 // 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
 import java.util.Scanner;
-public class MultiplicationAB {
+public class DivisionAB {
     public static void main(String[] args){
 // 입력
 // 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
