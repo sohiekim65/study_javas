7b5dea677164d10e9fd44f86c9b34bc0c60179b3
sohiekim65 <sohiekim65@gmail.com>
2022-10-15 13:05:33 +0900

    commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@
 // 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 // refer : https://www.acmicpc.net/problem/1000
 import java.util.Scanner;
-public class AddAB{
+public class HelloWorld{
     public static void main(String[] args){
 // 입력
 // 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
