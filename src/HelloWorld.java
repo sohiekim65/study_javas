8b398fa03315c5e8c9d850bdd85984999c147cc0
sohiekim65 <sohiekim65@gmail.com>
2022-10-15 13:08:21 +0900

    commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,18 +1,7 @@
 // 문제
-// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
-// refer : https://www.acmicpc.net/problem/1000
-import java.util.Scanner;
-public class HelloWorld{
-    public static void main(String[] args){
-// 입력
-// 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
-        Scanner in = new Scanner(System.in);
-        int A = in.nextInt();
-        int B = in.nextInt();
-        
-// 출력
-// 첫째 줄에 A+B를 출력한다.
-        System.out.println(A+B);
-        in.close();
+// Hello World!를 출력하시오.
+public class HelloWorld {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
     }
 }
