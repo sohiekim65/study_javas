26fc28881d34b222ef1b5773ba8bffc0914283a4
sohiekim65 <sohiekim65@gmail.com>
2022-11-08 18:45:27 +0900

    commit

--- src/MinusAB.java
+++ src/MinusAB.java
@@ -1,17 +1,38 @@
-// 문제
-// 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
 import java.util.Scanner;
+// 1. 문제 파악
+//  : 두 정수 A와 B를 입력받은 다음, A-B 출력
+
+// 2. 유추 파악
+//  : 두 정수를 입력해 뺄셈
+
+// 3. 주요 단어 영문 이름 선정
+//  : 입력 - input
+//    정수 - demical
+//    뺄셈 - minus, -
+//    출력 - output
+
 public class MinusAB {
+    // 두 정수 뺄셈
+    public int minus(int input_A, int input_B) {
+        int output_minus = input_A - input_B;
+        return output_minus;
+    }
     public static void main(String[] args){
-// 입력
-// 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
-        Scanner in = new Scanner(System.in);
-        int A = in.nextInt();
-        int B = in.nextInt();
+        // 4. 테스트 케이스
+        // 3 2 --> 1
+        // 1 1 --> 0
+
+        // 5. 프로그래밍 순서
+        // 입력
+        Scanner scanner = new Scanner(System.in);
+        int input_A = scanner.nextInt();
+        int input_B = scanner.nextInt();
+        scanner.close();
         
-// 출력
-// 첫째 줄에 A-B를 출력한다.
-        System.out.println(A-B);
-        in.close();
+        // 출력
+        // 첫째 줄에 A-B를 출력한다.
+        MinusAB minusAB = new MinusAB();
+        int output = minusAB.minus(input_A, input_B);
+        System.out.println(output);
     }
 }
--- src/MultiplicationAB.java
+++ src/MultiplicationAB.java
@@ -1,17 +1,38 @@
-// 문제
-// 두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
 import java.util.Scanner;
+// 1. 문제 파악
+//  : 두 정수 A와 B를 입력받은 다음, AxB 출력
+
+// 2. 유추 파악
+//  : 두 정수를 입력해 곱함
+
+// 3. 주요 단어 영문 이름 선정
+//  : 입력 - input
+//    정수 - demical
+//    곱합 - multiply, *
+//    출력 - output
 public class MultiplicationAB {
+    // 두 정수 곱함
+    public int multiply(int input_A, int input_B) {
+        int output_multiply = input_A*input_B;
+        return output_multiply;
+    }
     public static void main(String[] args){
-// 입력
-// 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
-        Scanner in = new Scanner(System.in);
-        int A = in.nextInt();
-        int B = in.nextInt();
+        // 4. 테스트 케이스
+        // 1 2 --> 2
+        // 3 4 --> 12
         
-// 출력
-// 첫째 줄에 A×B를 출력한다.
-        System.out.println(A*B);
-        in.close();
+        // 5. 프로그래밍 순서
+        // 입력
+        Scanner scanner = new Scanner(System.in);
+        int input_A = scanner.nextInt();
+        int input_B = scanner.nextInt();
+        scanner.close();
+        
+        // 처리
+        MultiplicationAB main = new MultiplicationAB();
+        int output = main.multiply(input_A, input_B);
+
+        // 출력
+        System.out.println(output);
     }
 }
