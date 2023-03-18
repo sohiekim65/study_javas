67838652ebb5f3d8c89b87bc3cc3c333d2647e23
sohiekim65 <sohiekim65@gmail.com>
2023-03-18 23:38:54 +0900

    return num

---
+++ src/codingTest/returnNum.java
@@ -0,0 +1,28 @@
+package codingTest;
+
+import java.util.Scanner;
+
+public class returnNum {
+    // 확인용
+    // public static void main(String[] args) {
+    //     Scanner scanner = new Scanner(System.in);
+    //     String input = scanner.next();
+    //     int answer = 0;
+    //     String[] num = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    //     for(int i = 0; i < 10; i++) {
+    //         input = input.replace(num[i], Integer.toString(i));
+    //     }
+    //     answer=Integer.parseInt(input);
+    //     System.out.println(answer);
+    // }
+
+    public int solution(String s) {
+        int answer = 0;
+        String[] num = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i = 0; i < 10; i++) {
+            s = s.replace(num[i], Integer.toString(i));
+        }
+        answer=Integer.parseInt(s);
+        return answer;
+    }
+}
